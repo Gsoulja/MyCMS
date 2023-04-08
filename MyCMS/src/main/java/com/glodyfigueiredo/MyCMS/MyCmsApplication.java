15db19dd6167bee218ecc48d19a007b670293eaf
Gsoulja <glodyfigueiredo@outlook.com>
2023-04-08 11:17:23 +0100

    Hello World

---
+++ MyCMS/src/main/java/com/glodyfigueiredo/MyCMS/MyCmsApplication.java
@@ -0,0 +1,14 @@
+package com.glodyfigueiredo.MyCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCmsApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
